2e3cdd8832d4ec7be52ea9709833b87df5f8d085
Diogo Andrade <diogomiguel1699@hotmail.com>
2024-04-23 18:13:17 +0100

    test2

--- projectstructure.c
+++ projectstructure.c
@@ -1,136 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-#define NUM_MAQUINAS 3
-#define NUM_TRABALHOS 3
-#define NUM_OPERACOES_MAX 3
+#define NUM_JOBS 3
+#define NUM_MACHINES 3
 
-// Estrutura para representar uma operação
-typedef struct {
-    int maquina;  // Número da máquina onde a operação será realizada
-    int duracao;  // Duração da operação
-} Operacao;
-
-// Estrutura para representar um trabalho
-typedef struct {
-    Operacao operacoes[NUM_OPERACOES_MAX];  // Array de operações para o trabalho
-} Trabalho;
-
-// Estrutura para representar o problema do Job-Shop
-typedef struct {
-    Trabalho trabalhos[NUM_TRABALHOS];  // Array de trabalhos
-} JobShop;
-
-// Função para inicializar o problema do Job-Shop com dados de exemplo
-void inicializarJobShopExemplo(JobShop *job_shop) {
-    // Exemplo de dados de entrada
-    int dados[NUM_TRABALHOS][NUM_OPERACOES_MAX][2] = {
+int main() {
+    // Definir a matriz de tempos de processamento para cada trabalho
+    int processing_times[NUM_JOBS][NUM_MACHINES][2] = {
         {{0, 3}, {1, 2}, {2, 2}},
         {{0, 2}, {2, 1}, {1, 4}},
         {{1, 4}, {2, 3}, {0, 1}}
     };
 
-    // Inicializar o problema do Job-Shop com os dados de exemplo
-    for (int i = 0; i < NUM_TRABALHOS; i++) {
-        for (int j = 0; j < NUM_OPERACOES_MAX; j++) {
-            job_shop->trabalhos[i].operacoes[j].maquina = dados[i][j][0];
-            job_shop->trabalhos[i].operacoes[j].duracao = dados[i][j][1];
-        }
-    }
-}
-
-// Função para exibir uma operação
-void mostrarOperacao(Operacao operacao, int inicio) {
-    printf("Maquina %d, Tempo de Inicio %d, Duracao %d\n", operacao.maquina, inicio, operacao.duracao);
-}
-
-// Função para exibir o problema do Job-Shop (apenas para fins de depuração)
-void mostrarJobShop(JobShop *job_shop) {
-    for (int i = 0; i < NUM_TRABALHOS; i++) {
-        printf("Trabalho %d:\n", i);
-        for (int j = 0; j < NUM_OPERACOES_MAX; j++) {
-            printf("  Operacao %d: ", j);
-            mostrarOperacao(job_shop->trabalhos[i].operacoes[j], 0);
-        }
-    }
-}
-
-// Função para calcular o tempo total de conclusão de um escalonamento
-int calcularTempoTotal(JobShop *job_shop) {
-    int tempo_total = 0;
-    int tempo_inicio[NUM_MAQUINAS] = {0}; // Tempo de início das operações em cada máquina
-
-    for (int i = 0; i < NUM_TRABALHOS; i++) {
-        for (int j = 0; j < NUM_OPERACOES_MAX; j++) {
-            int maquina = job_shop->trabalhos[i].operacoes[j].maquina;
-            int duracao = job_shop->trabalhos[i].operacoes[j].duracao;
-            int inicio = tempo_inicio[maquina];
-            tempo_inicio[maquina] += duracao; // Atualizar o tempo de início da próxima operação na máquina
-            tempo_total = (tempo_inicio[maquina] > tempo_total) ? tempo_inicio[maquina] : tempo_total;
-        }
-    }
-
-    return tempo_total;
-}
-
-// Função para calcular o próximo escalonamento
-int proximoEscalonamento(JobShop *job_shop) {
-    int i, j;
-
-    // Encontrar a próxima tarefa a ser trocada
-    for (i = NUM_TRABALHOS - 1; i >= 0; i--) {
-        for (j = NUM_OPERACOES_MAX - 1; j >= 0; j--) {
-            if (job_shop->trabalhos[i].operacoes[j].maquina < NUM_MAQUINAS - 1) {
-                job_shop->trabalhos[i].operacoes[j].maquina++;
-                return 1; // Encontrou um próximo escalonamento
-            } else {
-                job_shop->trabalhos[i].operacoes[j].maquina = 0;
+    // Imprimir os tempos de processamento de cada máquina para cada trabalho
+    printf("Tempos de processamento de cada máquina para cada trabalho:\n");
+    for (int i = 0; i < NUM_JOBS; i++) {
+        printf("J%d: ", i);
+        for (int j = 0; j < NUM_MACHINES; j++) {
+            printf("Machine%d duracao %d", processing_times[i][j][0], processing_times[i][j][1]);
+            if (j < NUM_MACHINES - 1) {
+                printf(", ");
             }
         }
+        printf("\n");
     }
 
-    return 0; // Não há mais escalonamentos possíveis
-}
+    // Calcular o somatório dos tempos de duração de cada máquina para todos os trabalhos
+    int machine_sums[NUM_MACHINES] = {0};
 
-// Função para exibir a matriz de soluções
-void mostrarMatriz(JobShop *job_shop) {
-    printf("\nMatriz de Solucoes:\n");
-    for (int i = 0; i < NUM_TRABALHOS; i++) {
-        printf("Trabalho %d:\n", i);
-        for (int j = 0; j < NUM_OPERACOES_MAX; j++) {
-            printf("  Operacao %d: Maquina %d, Duracao %d\n", j, job_shop->trabalhos[i].operacoes[j].maquina, job_shop->trabalhos[i].operacoes[j].duracao);
+    for (int i = 0; i < NUM_JOBS; i++) {
+        for (int j = 0; j < NUM_MACHINES; j++) {
+            machine_sums[processing_times[i][j][0]] += processing_times[i][j][1];
         }
     }
-    printf("\n");
-}
 
-int main() {
-    // Inicializar o gerador de números aleatórios
-    srand(time(NULL));
+    // Imprimir os somatórios do tempo de duração de cada máquina para todos os trabalhos
+    printf("\nSomatorios do tempo de duracao de cada maquina para todos os trabalhos:\n");
+    for (int i = 0; i < NUM_MACHINES; i++) {
+        printf("Machine%d total duration: %d\n", i, machine_sums[i]);
+    }
 
-    // Criar e inicializar o problema do Job-Shop
-    JobShop job_shop;
-    inicializarJobShopExemplo(&job_shop);
+     // Encontrar o valor máximo do somatório
+    int max_sum = machine_sums[0];
+    int max_machine = 0;
+    for (int i = 1; i < NUM_MACHINES; i++) {
+        if (machine_sums[i] > max_sum) {
+            max_sum = machine_sums[i];
+            max_machine = i;
+        }
+    }
+    printf("\nValor maximo do somatorio: %d\n", max_sum);
 
-    // Exibir o problema do Job-Shop (apenas para fins de depuração)
-    mostrarJobShop(&job_shop);
+    printf("A maquina com o maior somatorio e a Machine%d com um tempo total de %d.\n\n", max_machine, max_sum);
 
-    // Calcular e exibir todas as possíveis soluções e o tempo total de conclusão de cada uma
-    printf("Todas as possiveis solucoes e seus tempos totais de conclusao:\n");
-    clock_t start = clock();
-    int num_solucoes = 0;
-    do {
-        printf("Solucao %d:\n", num_solucoes + 1);
-        mostrarMatriz(&job_shop);
-        int tempo_total = calcularTempoTotal(&job_shop);
-        printf("Tempo total de conclusao: %d\n", tempo_total);
-        printf("-------------------------\n");
-        num_solucoes++;
-    } while (proximoEscalonamento(&job_shop));
-    clock_t end = clock();
-    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Tempo decorrido: %f segundos\n", time_spent);
 
-    return 0;
+
+
+    
 }
